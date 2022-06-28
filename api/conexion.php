b667795bd0f9fbd7f0df55d8c16153c56ce6fc5c
Alvii98 <alvaro.98@live.com.ar>
2022-06-28 18:13:11 -0300

    Se modifico archivo conexion

--- api/conexion.php
+++ api/conexion.php
@@ -1,13 +1,12 @@
 <?php
-	$database="epiz_30533450_canamo";
-	$user='epiz_30533450';
-	$password='b77PtRZ3AQmpX';
-
 
 try {
-	
-	$con=new PDO('mysql:host=localhost;dbname=carrito','root','');
-
+	//if($_SERVER[HTTP_HOST] == "canamogrowshop.42web.io"){
+	if(gethostname() == "freeweb10-1.byetcluster.com"){
+		$con=new PDO('mysql:host=sql308.epizy.com;dbname=epiz_30533450_canamo','epiz_30533450','b77PtRZ3AQmpX');
+	}else{
+		$con=new PDO('mysql:host=localhost;dbname=carrito','root','');
+	}
 } catch (PDOException $e) {
 	echo "Error".$e->getMessage();
 }
